701ed9ca9d8faaf14c90d5fdd90ab577cebb0ae5
201524554 <wjhh20022@gmail.com>
2021-12-17 15:59:52 +0900

    123

---
+++ src/components/TokenList.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Erc721 from "./Erc721";
+
+function TokenList({ web3, account, erc721list }) {
+    return (
+        <div className="tokenlist">
+            <Erc721 web3={web3} account={account} erc721list={erc721list} />
+        </div>
+    );
+}
+
+export default TokenList;
